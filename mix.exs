df2687163001c3da215f7f9609e2bb4430f07ed1
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2026-09-10 02:32:40 -0300

    chore(release): prepare beta.9

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Draught.MixProject do
   use Mix.Project
 
-  @version "0.1.0-beta.8"
+  @version "0.1.0-beta.9"
   @source_url "https://github.com/ccarvalho-eng/draught"
   @description "Provider-neutral coding-agent runtime and CLI for Elixir and the BEAM."
 
